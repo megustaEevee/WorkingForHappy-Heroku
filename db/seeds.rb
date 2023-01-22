4be3fd196bd061eeeefaee95f61c512c3ad61fbf
megustaEevee <71437195+megustaEevee@users.noreply.github.com>
2023-01-22 09:06:21 +0900

    出勤時間コメント表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+members = Member.create([{ name: 'お館様' }, { name: '炭治郎' }, { name: '善逸' }, { nami: '伊之助'}])
